0f5d22d58edbe215eee3c9332c8888c073365d94
TahirMansuri <tprocks786@gmail.com>
2022-10-20 10:15:53 +0530

    [ADDED]:Vipul C Program code for Addition of Two Numbers

---
+++ Add2No.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+	int a, b, c=0;
+	printf("\nEnter the First number=");
+	scanf("%d",&a);
+	printf("\nEnter the Second Number=");
+	scanf("%d",&b);
+	c=a+b;
+	printf("\nThe Addition of Two Numbers is=%d",c);
+}
